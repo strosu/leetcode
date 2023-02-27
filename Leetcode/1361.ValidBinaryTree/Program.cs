fe0a81cdfecd69bc1b404c31e9cbc8ede9efe74e
strosu <rosu@outlook.com>
2023-02-27 19:46:42 +0200

    Mathworks

---
+++ Leetcode/1361.ValidBinaryTree/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace _1361.ValidBinaryTree;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var r112 = new Solution().ValidateBinaryTreeNodes(4, new[] { 1, -1, 3, -1 }, new[] { 2, -1, -1, -1 }); // true
+        var r11 = new Solution().ValidateBinaryTreeNodes(4, new[] { 1, 0, 3, -1 }, new[] { -1, -1, -1, -1 }); // false
+        var r1 = new Solution().ValidateBinaryTreeNodes(4, new[] { 1, -1, 3, -1 }, new[] { 2, -1, -1, -1 }); // true
+        var r2 = new Solution().ValidateBinaryTreeNodes(4, new[] { 1, -1, 3, -1 }, new[] { 2, 3, -1, -1 }); // false
+        var r3 = new Solution().ValidateBinaryTreeNodes(2, new[] { 1, 0 }, new[] { 0, 1 }); // false
+
+        Console.ReadLine();
+    }
+
+    public class Solution
+    {
+        public bool ValidateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild)
+        {
+            var areLeftChildren = new HashSet<int>(leftChild);
+            var areRightChildren = new HashSet<int>(rightChild);
+
+            var roots = new List<int>();
+
+            for (var i = 0; i < n; i++)
+            {
+                if (!leftChild.Contains(i) && !areRightChildren.Contains(i))
+                {
+                    roots.Add(i);
+                }
+            }
+
+            if (roots.Count() != 1)
+            {
+                return false;
+            }
+
+            var visited = new HashSet<int>();
+            var nodeQueue = new Queue<int>();
+            nodeQueue.Enqueue(roots[0]);
+
+            while (nodeQueue.Any())
+            {
+                var currentNode = nodeQueue.Dequeue();
+
+                if (visited.Contains(currentNode))
+                {
+                    return false;
+                }
+
+                visited.Add(currentNode);
+
+                var left = leftChild[currentNode];
+                var right = rightChild[currentNode];
+
+                AddIfValid(nodeQueue, left);
+                AddIfValid(nodeQueue, right);
+            }
+
+            return visited.Count == n;
+        }
+
+        private void AddIfValid(Queue<int> queue, int current)
+        {
+            if (current >= 0)
+            {
+                queue.Enqueue(current);
+            }
+        }
+
+    }
+}
+
---
+++ Leetcode/Mathworks/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+
+namespace Mathworks;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var res1 = GetMinDivisor("bcdbcdbcdbcd", "bcdbcd"); // 3
+        var res2 = GetMinDivisor("bcdbcdbcd", "bcdbcd"); // -1
+
+        Console.ReadLine();
+    }
+
+    static int GetMinDivisor(string s, string t)
+    {
+        var smallStringCharrArray = t.ToCharArray();
+        var largeStringCharArray = s.ToCharArray();
+
+        if (!Divides(smallStringCharrArray, largeStringCharArray, smallStringCharrArray.Length))
+        {
+            return -1;
+        }
+
+        var divisorCandidate = new char[smallStringCharrArray.Length];
+
+        for (var i = 0; i < t.Length; i++)
+        {
+            divisorCandidate[i] = smallStringCharrArray[i];
+
+            if (Divides(divisorCandidate, smallStringCharrArray, i + 1))
+            {
+                return i + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    static bool Divides(char[] candidate, char[] targetString, int candidateSize)
+    {
+        if (targetString.Length % candidateSize != 0)
+        {
+            return false;
+        }
+
+        var candidateIndex = 0;
+        var targetStringIndex = 0;
+
+        while (targetStringIndex < targetString.Length)
+        {
+            if (candidate[candidateIndex] != targetString[targetStringIndex])
+            {
+                return false;
+            }
+
+            candidateIndex = (candidateIndex + 1) % candidateSize;
+            targetStringIndex++;
+        }
+
+        return true;
+    }
+}
+
