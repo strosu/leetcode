cb5fbc1877c6f74101374d2ad81a5de78941002c
strosu <rosu@outlook.com>
2023-03-06 09:08:43 +0200

    Game of life / knight dialer

---
+++ Leetcode/GameOfLife/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace GameOfLife
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static string[][] GetNextIterataion(string[][] current)
+        {
+            var result = new string[current.Length][];
+
+            for (var i = 0; i < current.Length; i++)
+            {
+                result[i] = new string[current[i].Length];
+
+                for (var j = 0; j < current[i].Length; j++)
+                {
+                    var states = GetNeighborStates(i, j, current);
+
+                    var currentlyAlive = current[i][j] == "x";
+
+                    if (currentlyAlive)
+                    {
+                        var aliveNeighbors = states.Count(x => x);
+
+                        if (aliveNeighbors >= 2 && aliveNeighbors <= 3)
+                        {
+                            result[i][j] = "x";
+                            continue;
+                        }
+
+                        result[i][j] = string.Empty;
+                        continue;
+                    }
+
+                    var deadNeighbors = states.Count(x => !x);
+                    if (deadNeighbors == 3)
+                    {
+                        result[i][j] = "x";
+                    }
+                    else
+                    {
+                        result[i][j] = string.Empty;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// True for alive, false otherwise
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="current"></param>
+        /// <returns></returns>
+        static List<bool> GetNeighborStates(int x, int y, string[][] current)
+        {
+            var neighbors = GetNeighbors(x, y, current.Length, current[0].Length);
+            return neighbors.Select(point => current[point.X][point.Y] == "x").ToList();
+        }
+
+        static List<Point> GetNeighbors(int x, int y, int maxRow, int maxColumn)
+        {
+            var candidates = new List<Point>();
+
+            candidates.Add(new Point(x - 1 , y - 1));
+            candidates.Add(new Point(x - 1 , y));
+            candidates.Add(new Point(x - 1 , y + 1));
+            
+            candidates.Add(new Point(x, y - 1));
+            candidates.Add(new Point(x, y + 1));
+
+            candidates.Add(new Point(x + 1, y - 1));
+            candidates.Add(new Point(x + 1, y));
+            candidates.Add(new Point(x + 1, y + 1));
+
+            return candidates.Where(point => IsWithinMatrix(point, maxRow, maxColumn)).ToList();
+        }
+
+        static bool IsWithinMatrix(Point point, int maxRow, int maxColumn)
+        {
+            return point.X >= 0 && point.X <= maxColumn
+                && point.Y >= 0 && point.Y <= maxRow;
+        }
+        class Point
+        {
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public int X { get; set; }
+
+            public int Y { get; set; }
+        }
+
+    }
+}
---
+++ Leetcode/KnightDialer/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Numerics;
+
+namespace KnightDialer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = GetDifferentCombinations(1, 1); // 1
+            var res2 = GetDifferentCombinations(6, 2); // 3
+            var res3 = GetDifferentCombinations(2, 3); // 4
+            var res4 = GetDifferentCombinations(2, 1000); // 4
+
+            Console.ReadLine();
+        }
+
+        static BigInteger GetDifferentCombinations(int startingDigit, int jumps)
+        {
+            var next = new Dictionary<int, List<int>>()
+            {
+                { 1, new List<int>() { 6, 8 } },
+                { 2, new List<int>() { 7, 9 } },
+                { 3, new List<int>() { 4, 8 } },
+                { 4, new List<int>() { 3, 9 } },
+                { 5, new List<int>() },
+                { 6, new List<int>() { 1, 7, 0 } },
+                { 7, new List<int>() { 2, 6 } },
+                { 8, new List<int>() { 1, 3 } },
+                { 9, new List<int>() { 4, 2 } },
+                { 0, new List<int>() { 4, 6 } }
+            };
+
+            var prevRow = new BigInteger[10];
+
+            for (var i = 0; i < 10; i++)
+            {
+                if (i != 5)
+                {
+                    prevRow[i] = 1;
+                }
+            }
+
+            for (var i = 1; i < jumps; i++)
+            {
+                var currentRow = new BigInteger[10];
+
+                for (var j = 0; j < 10; j++)
+                {
+                    var children = next[j];
+                    foreach (var child in children)
+                    {
+                        currentRow[j] += prevRow[child];
+                    }
+
+                }
+
+                Array.Copy(currentRow, prevRow, 10);
+            }
+
+            return prevRow[startingDigit];
+        }
+    }
+}
