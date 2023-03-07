3dcf48b5c10e1dda7b35a157c02d7ed85845ee64
strosu <rosu@outlook.com>
2023-03-07 10:52:48 +0200

    1.9.String rotation

--- Leetcode/Cracking/1.9.StringRotation/Program.cs
+++ Leetcode/Cracking/1.9.StringRotation/Program.cs
@@ -3,7 +3,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var isSubstring = IsSubstring("abcdefabbcd", "defa");
+        var isSubstring2 = IsSubstring("abcdefabbcd", "defbca");
+
+        var isRotation = IsRotation("abcdefghij", "defghijabc");
+        var isRotation2 = IsRotation("abcdefghij", "defghijabd");
+
+        Console.ReadLine();
+    }
+
+    static bool IsRotation(string first, string second)
+    {
+        return IsSubstring(first + first, second);
+    }
+
+    static bool IsSubstring(string longer, string shorter)
+    {
+        for (var i = 0; i < longer.Length - shorter.Length; i++)
+        {
+            var subStringLength = 0;
+            while (i + subStringLength < longer.Length && subStringLength < shorter.Length && longer[i + subStringLength] == shorter[subStringLength])
+            {
+                subStringLength++;
+            }
+
+            if (subStringLength == shorter.Length)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
 
