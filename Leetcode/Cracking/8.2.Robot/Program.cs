9c253f3e9e89e2d4ac826ee14ea1933895ca9564
strosu <rosu@outlook.com>
2023-03-17 13:36:29 +0200

    Cracking - recursive 1

---
+++ Leetcode/Cracking/8.2.Robot/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _8._2.Robot;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
---
+++ Leetcode/Cracking/81.Steps/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _81.Steps;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    static int CountSteps(int targetStep)
+    {
+        var counts = new int[targetStep];
+        counts[0] = 1;
+
+        for (var i = 1; i < targetStep; i++)
+        {
+            counts[i] = counts[i - 1];
+
+            if (i >= 2)
+            {
+                counts[i] += counts[i - 2];
+            }
+
+            if (i >= 3)
+            {
+                counts[i] += counts[i - 2];
+            }
+        }
+
+        return counts.Last();
+    }
+
+    static int CountStepsRecursively(int currentStep, int targetStep)
+    {
+        if (currentStep > targetStep)
+        {
+            return 0;
+        }
+
+        if (currentStep == targetStep)
+        {
+            return 1;
+        }
+
+        return CountStepsRecursively(currentStep + 1, targetStep)
+            + CountStepsRecursively(currentStep + 2, targetStep)
+            + CountStepsRecursively(currentStep + 3, targetStep);
+    }
+}
+
