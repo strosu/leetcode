2e1287b5bc828e378776a5b380be96f0b10398e5
strosu <rosu@outlook.com>
2023-07-16 15:02:31 +0200

    Algorithm design setup

---
+++ Algorithm design manual/algo-design/src/main/kotlin/1_2_RightJobs.kt
@@ -0,0 +1,10 @@
+class `1_2_RightJobs` {
+    fun compute(times: List<MovieProduction>) : Int {
+        return 11
+    }
+
+    data class MovieProduction(
+        val startTime: Int,
+        val endTime: Int) {
+    }
+}
